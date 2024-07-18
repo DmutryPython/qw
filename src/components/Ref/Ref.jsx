55e923ba91d27c26bf0d02bdea8cbd646090a233
DmutryPython <dimarubakov2007@gmail.com>
2024-07-18 17:50:08 +0300

    Update Ref.jsx

--- src/components/Ref/Ref.jsx
+++ src/components/Ref/Ref.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import axios from "axios"; // Импортируем Axios
 import './Ref.css';
 import { CopyToClipboard } from "react-copy-to-clipboard";
@@ -7,13 +6,10 @@ import { CopyToClipboard } from "react-copy-to-clipboard";
 const tg = window.Telegram.WebApp;
 
 const Ref = () => {
-    const navigate = useNavigate();
     const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(true); 
 
     const fetchData = async () => {
       try {
-        setLoading(true); // Устанавливаем состояние загрузки в true перед запросом 
         const response = await axios.post('https://preferably-engaging-grubworm.ngrok-free.app/ref', tg, {
           headers: {
             'Content-Type': 'application/json',
@@ -22,19 +18,11 @@ const Ref = () => {
         setData(response.data[1]); 
       } catch (error) {
         console.error('Ошибка:', error);
-      } finally {
-        setLoading(false); 
-      }
-    };  
+      } 
+      };  
 
     useEffect(() => {
       fetchData();  
-      const interval = setInterval(() => {
-        fetchData();
-      }, 1000000);    
-      return () => {
-        clearInterval(interval);
-      };
     }, []);
 
   
