a1f96368eecc353c232307b50b05321def0a0514
DmutryPython <dimarubakov2007@gmail.com>
2024-07-19 13:14:27 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import './App.css';
-import buttonpost from './components/button';
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
 import { Route, Routes} from 'react-router-dom';
 import Start from './components/Start/Start';
 import Major from './components/major/major';
@@ -13,15 +13,17 @@ function App() {
   
   
   return (
-    <div className="App">
-      123
-      <Header />
-      <Routes>
-        <Route index element={<Start />}/>
-        <Route path={'major'} element={<Major />}/>
-        <Route path={'ref'} element={<Ref />}/>
-      </Routes>
-    </div>
+    <TonConnectUIProvider manifestUrl="https://kripto123.netlify.app/tonconnect-manifest.json">
+      <div className="App">
+        123
+        <Header />
+        <Routes>
+          <Route index element={<Start />}/>
+          <Route path={'major'} element={<Major />}/>
+          <Route path={'ref'} element={<Ref />}/>
+        </Routes>
+      </div>
+    </TonConnectUIProvider>
   );
 }
 
