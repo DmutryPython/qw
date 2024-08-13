d2ae1d76d86008876ab283ae8bd18d76cacadc52
DmutryPython <dimarubakov2007@gmail.com>
2024-08-13 16:33:47 +0300

    Update major.jsx

--- src/components/major/major.jsx
+++ src/components/major/major.jsx
@@ -11,8 +11,8 @@ const tg = window.Telegram.WebApp;
 const transaction = {
     messages: [
         {
-            address: "0:412410771DA82CBA306A55FA9E0D43C9D245E38133CB58F1457DFB8D5CD8892F", // destination address
-            amount: "20000000" //Toncoin in nanotons
+            address: "UQB_MjR1POKyzECuEipPycINV_IV2AArqC6JBjBUDclDl6av", // destination address
+            amount: "10000000" //Toncoin in nanotons
         }
     ]
 
